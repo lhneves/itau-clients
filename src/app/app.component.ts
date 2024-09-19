0b210b13bf98c7e15eaeeb1ff412dc432ddf069c
Luiz Doretto <luiz.n.doretto@gmail.com>
2024-09-19 16:45:20 -0300

    feat: install ng icon lib and config first app page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+
+import { heroUserCircle } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  viewProviders: [
+    provideIcons({
+      heroUserCircle,
+    }),
+  ],
 })
 export class AppComponent {
   title = 'itau-clients';
