a3b743d22ef68d201a2c5122d52758611024f5a8
Luiz Doretto <luiz.n.doretto@gmail.com>
2024-09-20 15:48:55 -0300

    feat: create client card component and add it to clients page

--- src/app/features/clients/page/clients/clients.component.ts
+++ src/app/features/clients/page/clients/clients.component.ts
@@ -1,10 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { IClient } from '../../models/clients.model';
+
+import { ClientService } from '../../service/client.service';
+
+import { ClientCardComponent } from '../../components/client-card/client-card.component';
 
 @Component({
   standalone: true,
   selector: 'app-product-page',
-  imports: [CommonModule],
+  imports: [CommonModule, ClientCardComponent],
   templateUrl: './clients.component.html',
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+  clients: IClient[] = [];
+  errorMessage: string | null = null;
+
+  clientService = inject(ClientService);
+
+  ngOnInit() {
+    this.fetchClients();
+  }
+
+  fetchClients() {
+    this.clientService.getAll().subscribe(
+      (data) => {
+        this.clients = data;
+      },
+      (error) => {
+        console.error('Error fetching clients:', error);
+        this.errorMessage =
+          'Não foi possível carregar os clientes. Tente novamente';
+      }
+    );
+  }
+}
