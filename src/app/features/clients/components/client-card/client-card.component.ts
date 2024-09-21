3c88bfc94291075507c56de45372fbc56284a1da
Luiz Doretto <luiz.n.doretto@gmail.com>
2024-09-21 18:09:11 -0300

    feat: add confirmation dialog for client delete

--- src/app/features/clients/components/client-card/client-card.component.ts
+++ src/app/features/clients/components/client-card/client-card.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { NgIconComponent } from '@ng-icons/core';
 
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
+import { ClientService } from '../../service/client.service';
+import { ConfirmationService } from 'primeng/api';
 
 import { IClient } from '../../models/clients.model';
 
@@ -17,10 +21,41 @@ import { IClient } from '../../models/clients.model';
     RouterOutlet,
     RouterLink,
     ButtonModule,
+    ConfirmDialogModule,
   ],
+  providers: [ConfirmationService],
   templateUrl: './client-card.component.html',
   styleUrl: './client-card.component.scss',
 })
 export class ClientCardComponent {
   @Input() clientInfo!: IClient;
+  @Output() deleteEvent = new EventEmitter();
+
+  clientService = inject(ClientService);
+  confirmationService = inject(ConfirmationService);
+
+  confirmDelete(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Tem certeza que deseja deletar esse cliente?',
+      header: 'Confirmação',
+      icon: 'none',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text mr-2 p-button-lg',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+        this.handleDeleteProduct(this.clientInfo.codigo_cliente ?? '');
+      },
+    });
+  }
+
+  handleDeleteProduct(clientCode: string) {
+    this.clientService.delete(clientCode).subscribe((response) => {
+      if (response.error) return;
+
+      this.deleteEvent.emit();
+    });
+  }
 }
